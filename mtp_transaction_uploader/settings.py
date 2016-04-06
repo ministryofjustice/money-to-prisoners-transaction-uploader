cecb9647deb9106ccb47d8976fb306815b5a7e59
Ian Brechin <ian.brechin@gmail.com>
2016-04-06 15:49:49 +0100

    Create functional tests for file upload

--- mtp_transaction_uploader/settings.py
+++ mtp_transaction_uploader/settings.py
@@ -10,10 +10,10 @@ SFTP_HOST = os.environ.get('SFTP_HOST', '')
 SFTP_USER = os.environ.get('SFTP_USER', '')
 SFTP_PRIVATE_KEY = os.environ.get('SFTP_PRIVATE_KEY', '~/.ssh/id_rsa')
 SFTP_DIR = os.environ.get('SFTP_DIR', '')
-ACCOUNT_CODE = os.environ.get('ACCOUNT_CODE', '')
+ACCOUNT_CODE = os.environ.get('ACCOUNT_CODE', '444444')
 
-API_USERNAME = os.environ.get('API_USERNAME', 'transaction-uploader')
-API_PASSWORD = os.environ.get('API_PASSWORD', 'notthepassword')
+API_USERNAME = os.environ.get('API_USERNAME', 'bank-admin')
+API_PASSWORD = os.environ.get('API_PASSWORD', 'bank-admin')
 
 API_CLIENT_ID = os.environ.get('API_CLIENT_ID', 'bank-admin')
 API_CLIENT_SECRET = os.environ.get('API_CLIENT_SECRET', 'bank-admin')
