e4818211923ecd4936af4120fb64f71b7b0fd587
Igor <igor.ushkarev@digital.justice.gov.uk>
2016-04-07 10:33:58 +0100

    Add functional test command to `run.sh` but maybe run.sh should be replaced with a Makefile for consistency

--- run.sh
+++ run.sh
@@ -5,6 +5,7 @@ cd `dirname $0`
 print_usage() {
   echo "Usage: $0 [test|update] [args]"
   echo "- test [tests]: run the tests"
+  echo "- test-all [tests]: run all tests, including functional tests"
   echo "- update: update the virtual environment"
   exit 1
 }
@@ -31,6 +32,11 @@ run_tests() {
   nosetests $@
 }
 
+run_all_tests() {
+  # run all python tests, including functional tests
+  RUN_FUNCTIONAL_TESTS=1 OAUTHLIB_INSECURE_TRANSPORT=1 nosetests $@
+}
+
 main() {
   ACTION=$1
   shift
@@ -39,6 +45,11 @@ main() {
     test)
     activate_venv
     run_tests $@
+    ;;
+
+    test-all)
+    activate_venv
+    run_all_tests $@
     ;;
 
     update)
